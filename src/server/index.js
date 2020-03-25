6c67d862d7d26c13fe1e877c0a4accb3759e5bb1
Marius CONJEAUD <m.conjeaud@groupeonepoint.com>
2020-03-25 15:24:54 +0100

    Fix build output folder

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
 
 const dist = process.env.DISTLOCATION || '../../dist/testapp';
 const app = express();
 
-app.use(express.static(dist));
+app.use(express.static(__dirname));
 app.get('*', (req, res) => {
-  res.sendFile(`index.html`, { root: dist });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.SERVER_PORT || '3000';
-app.listen(port, () => console.log(`App running on port ${port}`));
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`App running on port ${port}`));
