d7c1abb82298cbc0a9bab69d4fa024b6173a4d4b
Marius CONJEAUD <m.conjeaud@groupeonepoint.com>
2020-04-06 12:29:47 +0200

    Fix e2e testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('A superb app');
+    expect(page.getTitleText()).toEqual('My awesome employees :');
   });
 
   afterEach(async () => {
